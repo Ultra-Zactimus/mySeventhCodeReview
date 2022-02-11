b0665118689012d914de0773b0265ab0a8e05fad
Ultra Zacky <ultrazacky@gmail.com>
2022-02-11 13:59:02 -0800

    changed file names, things are broken

--- src/index.js
+++ src/index.js
@@ -2,11 +2,7 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import Euro from './js/euro.js';
-import Yen from './js/euro.js';
-import Rupee from './js/euro.js';
-import Peso from './js/euro.js';
-import Ruble from './js/euro.js';
+import MoneyExchange from './js/euro.js';
 
 function clearResults () {
     $('#userInput').val("");
@@ -16,57 +12,36 @@ function clearResults () {
     $('#peso').text("");
     $('#ruble').text("");
     $('#error').text("");
-    $('#typeErr1').text("");
-    $('#typeErr2').text("");
 }
 
 function getResults(response) {
   if (response.result === "success") {
-    // if (response.target_code === "EUR") {
+    if (response.target_code === "EUR") {
       $('#euro').text(`The conversion is ${response.conversion_result} EUR`);
-    // } else if (response.target_code === "JPY") {
-    //   $('#yen').text(`The conversion is ${response.conversion_result} JPY`);
-    // } else if (response.target_code === "IRN") {
-    //   $('#rupee').text(`The conversion is ${response.conversion_result} INR`);
-    // } else if (response.target_code === "MXN") {
-    //   $('#peso').text(`The conversion is ${response.conversion_result} MXN`);
-    // } else if (response.target_code === "RUB") {
-    //   $('#ruble').text(`The conversion is ${response.conversion_result} RUB`);
-    // } 
+    } else if (response.target_code === "JPY") {
+      $('#yen').text(`The conversion is ${response.conversion_result} JPY`);
+    } else if (response.target_code === "INR") {
+      $('#rupee').text(`The conversion is ${response.conversion_result} INR`);
+    } else if (response.target_code === "MXN") {
+      $('#peso').text(`The conversion is ${response.conversion_result} MXN`);
+    } else if (response.target_code === "RUB") {
+      $('#ruble').text(`The conversion is ${response.conversion_result} RUB`);
+    } 
   } else {
-    $('#error').text(`(<error>) ${response} (<error>)`);
+    $('#error').text(`(<error>) ${response.error_type} (<error>)`);
     }
 }
 
 async function apiCall(input, exchange) {
-  
-    const euro = await Euro.moneyConverter(input, exchange);
-    getResults(euro);
-
-  // async function yen(input) {
-  //   const yen = await Yen.moneyConverter(input);
-  //   getResults(yen);
-  // }
-  // async function rupee(input) {
-  //   const rupee = await Rupee.moneyConverter(input);
-  //   getResults(rupee);
-  // }
-  // async function peso(input) {
-  //   const peso = await Peso.moneyConverter(input);
-  //   getResults(peso);
-  // }
-  // async function ruble(input) {
-  //   const ruble = await Ruble.moneyConverter(input);
-  //   getResults(ruble);
-  // }
+    const response = await MoneyExchange.moneyConverter(input, exchange);
+    getResults(response);
 }
 
 $(document).ready(function() {
   $('#button').on('click', function() {
     let input = parseInt($('#usd').val());
-    console.log(input);
     let exchange = $('#selector').val();
-    console.log(exchange);
+    clearResults();
     apiCall(input, exchange);
   });
 });
--- src/js/exchange.js
+++ src/js/exchange.js
@@ -1,4 +1,4 @@
-export default class Euro {
+export default class MoneyExchange {
   static async moneyConverter(input, exchange) {
     try {
       const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${exchange}/${input}`);
