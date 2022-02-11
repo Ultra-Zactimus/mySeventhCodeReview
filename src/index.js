a41a7b0e9010962469a6474d4962ae637c266846
Ultra Zacky <ultrazacky@gmail.com>
2022-02-11 09:50:43 -0800

    add apiCall() and document ready

--- src/index.js
+++ src/index.js
@@ -6,7 +6,11 @@ import Exchange from './js/exchange.js';
 
 function clearResults () {
     $('#userInput').val("");
-    $('#currency').text("");
+    $('#euro').text("");
+    $('#yen').text("");
+    $('#rupee').text("");
+    $('#peso').text("");
+    $('#ruble').text("");
     $('#error').text("");
     $('#typeErr1').text("");
     $('#typeErr2').text("");
@@ -14,10 +18,29 @@ function clearResults () {
 
 function grabResults(response) {
   if (response.main) {
-    $('#currency').text(`The conversion for ${input} ${respsonse.base_code} is ${conversion}`);
+    $('#euro').text(`The conversion for ${input} USD is ${response.conversion_rates.EUR} EUR`);
+    $('#yen').text(`The conversion for ${input} USD is ${response.conversion_rates.JPY} JPY`);
+    $('#rupee').text(`The conversion for ${input} USD is ${response.conversion_rates.INR} INR`);
+    $('#peso').text(`The conversion for ${input} USD is ${response.conversion_rates.MXN} MXN`);
+    $('#ruble').text(`The conversion for ${input} USD is ${response.conversion_rates.RUB} RUB`);
   } else {
     $('#error').text(`(<error>) ${response} (<error>)`);
-    $('#typeErr1').text(`Error 404 Not Found: "This means you have an invalid input."`);
+    $('#typeErr1').text(`Error 404 Not Found: "This means you have entered an invalid input."`);
     $('#typeErr2').text(`Error 401 Unauthorized: "This means your API Key is invalid."`);
   }
 }
+
+async function apiCall(input, currency) {
+  const response = await Exchange.moneyConverter();
+  grabResults(response);
+}
+
+$(document).ready(function() {
+  $('#selector').on('change', function(){
+    let currency = $('#currency').val();
+    $('#button').on('click', function(){
+      let input = $('#usd').val();
+      apiCall(input, currency);
+    });
+  });
+});
--- src/js/exchange.js
+++ src/js/exchange.js
@@ -1,7 +1,7 @@
 export default class Exchange {
-  static async moneyConverter(money) {
+  static async moneyConverter() {
     try {
-      const response = await fetch(`https://v6.exchangerate-api.com/v6/${API_KEY}/latest/${country}`);
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${API_KEY}/latest/USD`);
       if (!response.ok) {
         throw Error(response.status + " " + response.statusText);
       } return response.json();
